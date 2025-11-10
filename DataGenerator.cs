ced029d6f433b8246ec21cc456b2d47f0f05ec05
Ievlev Yegor <ievlevegor03.taxix@gmail.com>
2025-11-10 16:02:33 +0300

    Основной функционал:
    + Кошелёк
    + Транзакция
    + Генератор данных
    + Валидатор

---
+++ DataGenerator.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wallets
+{
+	/// <summary>
+	/// Генератор случайных последовательностей данных разных типов и объектов.
+	/// </summary>
+	public static class DataGenerator
+	{
+		#region EdgeConstants
+		public const int lowestIncome = 50;
+
+		public const int highestIncome = 1000;
+
+		public const int lowestExpense = 10;
+
+		public const int highestExpense = 800;
+
+		public const int lowestInitialBalance = 500;
+
+		public const int highestInitialBalance = 5000;
+		#endregion
+
+		/// <summary>
+		/// Генерирует список транзакций размера <paramref name="size"/> от заданной даты 
+		/// <paramref name="referenceDate"/> до текущей.
+		/// </summary>
+		/// <param name="size"> Количество транзакций. </param>
+		/// <param name="referenceDate"> Опорная дата. </param>
+		/// <returns> Список транзакций. </returns>
+		public static List<Transaction> GenerateTransactions(int size, DateTime referenceDate)
+		{
+			var random = new Random();
+			
+			var currentDate = DateTime.Now;
+			var daysFromReference = (int)(currentDate - referenceDate).TotalDays;
+
+
+			return Enumerable.Range(0, size)
+				.Select(x =>
+				{
+					var isIncome = random.Next(0, 2) == 0;
+					var amount = isIncome ? random.NextDouble() * highestIncome + lowestIncome 
+						: random.NextDouble() * highestExpense + lowestExpense;
+
+					var descriptions = isIncome ? 
+						new string[] { "Пополнение счёта.", "Входящий перевод.", "Заработная плата", "Возврат средств." } :
+						new string[] { "Оплата товаров.", "Перевод средств.", "Оплата услуг.", "Автоплатёж." };
+
+					return new Transaction(
+					   DateTime.Now.AddDays(-random.Next(0, daysFromReference)),
+					   amount,
+					   isIncome ? TransactionType.Income : TransactionType.Expense,
+					   $"{descriptions[random.Next(descriptions.Length)]}"
+					   );
+				}
+				).ToList();
+		}
+
+		/// <summary>
+		/// Генерирует список кошельков размера <paramref name="size"/>.
+		/// </summary>
+		/// <param name="size"> Количество кошельков. </param>
+		/// <returns> Список кошельков. </returns>
+		public static List<Wallet> GenerateWallets(int size)
+		{
+			var random = new Random();
+
+			var walletNames = new string[] { "Qiwi", "Yandex", "Payoneer", "WebMoney" };
+			var currencies = new string[] { "Dollar", "Euro", "Ruble", "Lari" };
+
+			return Enumerable.Range(0, size)
+				.Select(x =>
+					 new Wallet(
+						$"{walletNames[random.Next(walletNames.Length)]}",
+						$"{currencies[random.Next(currencies.Length)]}",
+						random.NextDouble() * highestInitialBalance + lowestInitialBalance
+						)
+				).ToList();
+		}
+	}
+}
---
+++ Wallet.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wallets
+{
+	/// <summary>
+	/// Кошелёк.
+	/// </summary>
+	public class Wallet
+	{
+		#region Fields
+		/// <summary>
+		/// Идентификатор.
+		/// </summary>
+		public Guid WalletId { get; }
+
+		/// <summary>
+		/// Название кошелька.
+		/// </summary>
+		public string Name { get; }
+
+		/// <summary>
+		/// Валюта.
+		/// </summary>
+		public string Currency { get; }
+
+		/// <summary>
+		/// Начальный баланс.
+		/// </summary>
+		public double InitialBalance { get; }
+
+		/// <summary>
+		/// Текущий баланс.
+		/// </summary>
+		public double CurrentBalance { get; private set; }
+
+		/// <summary>
+		/// Список транзакций.
+		/// </summary>
+		private List<Transaction> _transactions { get; set; }
+		#endregion
+
+		/// <summary>
+		/// Инициализирует все поля, создаёт идентификатор.
+		/// </summary>
+		/// <param name="name"> Название кошелька. </param>
+		/// <param name="currency"> Валюта. </param>
+		/// <param name="initialBalance"> Начальный баланс. </param>
+		public Wallet(string name, string currency, double initialBalance)
+		{
+			WalletId = Guid.NewGuid();
+			
+			Name = name;
+			
+			Currency = currency;
+			
+			InitialBalance = initialBalance;
+			CurrentBalance = initialBalance;
+
+			_transactions = new List<Transaction>();
+		}
+
+		/// <summary>
+		/// Вычисляет текущий баланс.
+		/// </summary>
+		/// <returns> Текущий баланс. </returns>
+		public double CalculateCurrentBalance()
+		{
+			CurrentBalance = InitialBalance;
+
+			foreach (var transaction in _transactions)
+			{
+				CurrentBalance += (transaction.Type == TransactionType.Income
+					? transaction.Amount : transaction.Amount * -1);
+			}
+
+			return CurrentBalance;
+		}
+
+		/// <summary>
+		/// Проводит транзакцию <paramref name="transaction"/> согласно её типу.
+		/// </summary>
+		/// <param name="transaction"> Транзакция дохода или расхода. </param>
+		public void ConductTransaction(Transaction transaction)
+		{
+			if (transaction.Type == TransactionType.Income)
+			{
+				CurrentBalance += transaction.Amount;
+
+				_transactions.Add(transaction);
+			}
+			else
+			{
+				try
+				{
+					NumberValidator.IsNotNegativeDifference(CurrentBalance, transaction.Amount);
+
+					CurrentBalance -= transaction.Amount;
+
+					_transactions.Add(transaction);
+				}
+				catch (ArgumentException ex)
+				{
+					Console.WriteLine(ex.ToString());
+					Console.WriteLine($"Транзакция [{transaction.TransactionId}] не была проведена в кошельке [{WalletId}]!\n");
+				}
+			}
+		}
+
+		/// <summary>
+		/// Возвращает информацию о кошельке и его транзакциях в виде строки.
+		/// </summary>
+		/// <returns> Строка сведений об объекте. </returns>
+		public string GetInfo()
+		{
+			var info = new StringBuilder(
+				$"Кошелёк [{WalletId}] - имя [{Name}] - валюта [{Currency}]\n" +
+				$"Исходный баланс [{InitialBalance:f2}] - текущий баланс [{CalculateCurrentBalance():f2}]\n" +
+				$"\nСписок транзакций кошелька:\n"
+				);
+
+			for(var i = 0; i < _transactions.Count(); i++)
+			{
+				info.AppendLine($"{i + 1}. \t" + _transactions[i].GetInfo());
+			}
+
+			info.AppendLine();
+			
+			return info.ToString();
+		}
+	}
+}
